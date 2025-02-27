90ea7f70b5a3e5bc0011d2b3959364052dfa3562
Alex <qwertyuasfxxxafd8@gmail.com>
2025-02-27 15:51:54 +0300

    some pd.DataFrame is added

--- py_script.py
+++ py_script.py
@@ -1,2 +1,8 @@
+import pandas as pd
+
 string = 'When the levee breaks'
 print(f'{string}\nI have no place to stay')
+
+
+df = pd.DataFrame((1, 'When'), (2, 'the'), (3, 'levee'), (4, 'breaks'))
+print(df)
